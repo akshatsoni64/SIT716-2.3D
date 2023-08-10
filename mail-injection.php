7a67c7a7ba1faa29284f29c36b984b65b005bcb2
monster <akshatsoni64@gmail.com>
2023-08-10 16:38:40 +1000

    Added Mail Scripts

---
+++ mail-injection.php
@@ -0,0 +1,20 @@
+<?php
+$mailto = "akshat@example.com\ncc:alex@example.com";
+
+$mailSub = "Subject";
+$mailMsg = "Message";
+
+$headers[] = 'From: Admin <admin@example.com>';
+$headers[] = 'Bcc: soni@example.com';
+
+$mail = mail($mailto, $mailSub, $mailMsg, implode("\r\n", $headers));
+echo $mail."\n";
+
+if (!$mail) {
+    echo "Mail Not Sent";
+}
+else {
+    echo "Mail Sent";
+}
+
+echo "\n\n";
